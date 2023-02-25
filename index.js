57ccd81d1facc0efd1e156b2962db55ac572dccf
Samama Aziz <122434021+Samama-Aziz@users.noreply.github.com>
2023-02-25 19:25:15 +0500

    strength bar added

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let reload_btn = document.getElementById("reload_btn")
 let copy_btn = document.getElementById("copy_btn");
 let input_number = document.getElementById("numberfeild");
@@ -8,6 +7,7 @@ let Uppercase = document.getElementById("Uppercase");
 let Numbers = document.getElementById("Numbers");
 let Symbols = document.getElementById("Symbols");
 let Password_display = document.getElementById("password")
+let strenghtbar = document.getElementById("strenght_bar");
 
 input_number.defaultValue = 8;
 input_range.defaultValue = 8;
@@ -27,7 +27,6 @@ const Numbers_array = arrayFromLowToHigh(48, 57);
 const Symbols_array = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
 
 window.onload = password_caller();
-
 
 
 let rotatedeg = 0;
@@ -53,8 +52,11 @@ function password_caller() {
   const isnumbers = Numbers.checked;
   const issymbols = Symbols.checked;
 
-  const Password = generate_password(length, isupercase, islowercase, isnumbers, issymbols);
-  Password_display.value=Password;
+  const password = generate_password(length, isupercase, islowercase, isnumbers, issymbols);
+  Password_display.value = password;
+
+  const score = findscore(password);
+  barlenght(score);
 }
 
 function generate_password(length, isupercase, islowercase, isnumbers, issymbols) {
@@ -89,17 +91,63 @@ function arrayFromLowToHigh(low, high) {
 function copypassword() {
 
   Password_display.select();
-  Password_display.setSelectionRange(0, 99999); // For mobile devices
+  Password_display.setSelectionRange(0, 99999);
   navigator.clipboard.writeText(Password_display.value);
   alert("Copied the Password: " + Password_display.value);
-  
-document.getSelection().collapseToEnd()
-  
+
+  document.getSelection().collapseToEnd()
+
 }
 
-function generate(){
+function generate() {
   setTimeout(() => { copypassword(); }, 500);
   password_caller();
 
+}
+
+function findscore(password) {
+
+  var strength = 0;
+  if (password.length > 7) {
+    strength += 1;
+  }
   
+
+  if (password.match(/[a-z]/) && password.match(/[A-Z]/)) {
+    strength += 1;
+  }
+
+  if (password.match(/\d/)) {
+    strength += 1;
+  }
+
+  if (password.match(/[^a-zA-Z\d]/)) {
+    strength += 1;
+  }
+  return strength;
+}
+
+function colorPicker(score) {
+  switch (score) {
+    case 1:
+      strenghtbar.style.backgroundColor = "#E22800";
+      break;
+    case 2:
+      strenghtbar.style.backgroundColor = "#E28C00";
+      break;
+    case 3:
+      strenghtbar.style.backgroundColor = "#F7C703 ";
+      break;
+    case 4:
+      strenghtbar.style.backgroundColor = "#1AE505";
+      break;
+
+  }
+}
+function barlenght(score) {
+  const Score = (score * 25);
+
+  strenghtbar.style.width = Score + "%";
+  colorPicker(score);
+  strenghtbar.style.transition = "all 0.75s"
 }
